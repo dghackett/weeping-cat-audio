0d542639065526aa206497278ed5aa8a190ad68b
Dylan Hackett-Murgas <hackettdg@gmail.com>
2021-07-19 00:06:12 -0700

    updated logo

--- weeping-cat-landing-page-master/src/Components/Title/TitleMar21.js
+++ weeping-cat-landing-page-master/src/Components/Title/TitleMar21.js
@@ -3,7 +3,7 @@ import '../../App.css';
 import React from 'react';
 import { Component } from 'react';
 import { Container, Col, Row } from 'react-bootstrap';
-import weepingCatHeader from '../../images/WeepingCatTitle/weepingcatheader_reposition.png';
+import weepingCatHeader from '../../images/WeepingCatTitle/weepingcatheader_Jul2021.png';
 import threeStars from '../../images/WeepingCatTitle/three-stars_reposition.png';
 
 export class TitleMar21 extends Component{ 
@@ -16,9 +16,9 @@ export class TitleMar21 extends Component{
     <Col lg={6} md={6} sm={8} xs={8} className=""> 
       <img src={weepingCatHeader} alt="weepingCatHeader" className="img-fluid"/> 
     </Col>
-    <Col className="adjust-position">
+            <Col className="adjust-position">
       <img src={threeStars} alt="threeStars" className="shrink-stars adjust-position anchor-right"/>
-    </Col>
+    </Col> 
   </div>
 </div>
             </div>
